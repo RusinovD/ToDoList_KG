4318858265c90e4150232463d0ca0f7e590e858d
RusinovD <rusinovdu@bk.ru>
2025-03-07 20:51:05 +0500

    Добавил методы в UserInteraction

--- src/main/java/ToDoList/UserInteraction.java
+++ src/main/java/ToDoList/UserInteraction.java
@@ -1,5 +1,24 @@
 package ToDoList;
 
-public class UserInteraction {
+public class UserInteraction implements UserInteractionInterface {
 
+    @Override
+    public void scanName() {
+
+    }
+
+    @Override
+    public void scanDescription() {
+
+    }
+
+    @Override
+    public void scanDeadline() {
+
+    }
+
+    @Override
+    public void scanStatus() {
+
+    }
 }
